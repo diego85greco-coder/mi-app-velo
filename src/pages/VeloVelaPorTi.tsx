c1163a66f0abf83c9df09d36d854a9d279d4788d
diego85greco-coder <diego85greco@gmail.com>
2026-05-15 10:13:49 +0100

    Create VeloVelaPorTi component with project info

---
+++ src/pages/VeloVelaPorTi.tsx
@@ -0,0 +1,8 @@
+export default function VeloVelaPorTi() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1 style={{ color: '#7c3aed' }}>Velo Vela Por Ti</h1>
+      <p>Información sobre el proyecto y privacidad.</p>
+    </div>
+  );
+}
