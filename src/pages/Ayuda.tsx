ffa904388bcaa636921b260dbe27623083599e71
diego85greco-coder <diego85greco@gmail.com>
2026-05-15 10:11:50 +0100

    Create Ayuda.tsx

---
+++ src/pages/Ayuda.tsx
@@ -0,0 +1,8 @@
+export default function Ayuda() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1 style={{ color: '#dc2626' }}>Centro de Ayuda</h1>
+      <p>Botón de pánico y números de emergencia.</p>
+    </div>
+  );
+}
