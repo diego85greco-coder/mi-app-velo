818de140024a5841c3a70c84feaff7a1576a4ed5
diego85greco-coder <diego85greco@gmail.com>
2026-05-15 10:12:26 +0100

    Create Circulos component for displaying circles

---
+++ src/pages/Circulos.tsx
@@ -0,0 +1,8 @@
+export default function Circulos() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1 style={{ color: '#ca8a04' }}>Mis Círculos</h1>
+      <p>Grupos y comunidades de apoyo.</p>
+    </div>
+  );
+}
