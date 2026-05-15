db85f4dc77159f6106af633e73213c091991da8b
diego85greco-coder <diego85greco@gmail.com>
2026-05-15 10:13:07 +0100

    Add Contacto component for support form

---
+++ src/pages/Contacto.tsx
@@ -0,0 +1,8 @@
+export default function Contacto() {
+  return (
+    <div style={{ padding: '20px', textAlign: 'center' }}>
+      <h1 style={{ color: '#ea580c' }}>Contacto</h1>
+      <p>Formulario de soporte y sugerencias.</p>
+    </div>
+  );
+}
